85d56016bbfdabba1f6e6d82157d9cef1532b58b
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-05-08 10:25:02 +0200

    clean code

--- function.php
+++ function.php
@@ -1,13 +1,7 @@
 <?php
-
-$length = $_GET['pass_length'];
-
 // generate pss func
-function generate_password($length)
+function generate_password($length, $char)
 {
-   if (!empty($_GET['pass_length'])) {
-      $char = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz~!@#$%^&*()_-+={[}]|\:;".?/<>,';
-      return substr(str_shuffle($char), 0, $length);
-   }
+   return substr(str_shuffle($char), 0, $length);
 }
 ?>
